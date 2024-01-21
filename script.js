d3490dd1a1727fe79547867b24435ba6c49b30b3
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-01-20 21:01:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var mymap = L.map('mapid').setView([38.6, -90.2], 9);
 
 //stadia.outdoors basemap
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
 	minZoom: 0,
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
