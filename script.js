8d4f7589cc54b0570b3e81d0db4c688eefb189e0
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-01-22 20:46:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 //STL is 38.62663502630333, -90.22593800714141
 var mymap = L.map('mapid').setView([38.6, -90.2], 9);
 
-//stadia.outdoors basemap
-L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+//basemap
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
 // Hi-Pointe locations
