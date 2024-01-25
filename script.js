543b7d7f16e4a3e9c5f303155aa60636e321d0fa
Emma Rosenthal <124089752+emmarose-27@users.noreply.github.com>
2024-01-24 19:36:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //STL is 38.62663502630333, -90.22593800714141
 var mymap = L.map('mapid').setView([38.6, -90.2], 9);
 
-//basemap
+//stadia.outdoors basemap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -10,6 +10,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // Hi-Pointe locations
 //Main
 var marker1 = L.marker([38.63216702327716, -90.30549277390722]).addTo(mymap);
+// no idea what is meant by "change the symbology for the points", can't figure out how to change properties of the marker itself, so here's a circle
+var circle = L.circle([38.63216702327716, -90.30549277390722], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+    }).addTo(mymap);
 //Downtown
 var marker2 = L.marker([38.6302982032247, -90.19033437992162]).addTo(mymap);
 //Kirkwood
